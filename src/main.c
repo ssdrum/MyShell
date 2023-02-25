b55cc85b77e827f50aba5f7691097b2b416d5054
ssdrum <dipaololuigi95@gmail.com>
2023-02-25 12:51:05 +0000

    added gitignore

---
+++ src/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "my-shell.h"
+
+
+int main(int argc, char *argv[]) {
+    printf("%i\n", MAX_ARGS);
+    return 0;
+}
---
+++ src/my-shell.h
@@ -0,0 +1,2 @@
+#define MAX_BUFFER 1024
+#define MAX_ARGS 64
